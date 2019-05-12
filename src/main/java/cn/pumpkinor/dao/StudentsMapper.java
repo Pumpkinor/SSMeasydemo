e98c29b17b73c16f5b0ffd9b3dd5f696b705ec3b
pumpkinor <pumpkinor@163.com>
2019-05-12 18:19:30 +0800

    nothing

--- src/main/java/cn/pumpkinor/dao/StudentsMapper.java
+++ src/main/java/cn/pumpkinor/dao/StudentsMapper.java
@@ -7,10 +7,11 @@ import java.util.List;
 /**
  * @author Pumpkinor
  */
-@Repository
 //这个注解是将接口的一个实现类(具体这个实现类的name叫什么,还需要再分析源码找找看)
 // 交给spring管理(在spring中有开启对@repository注解的扫描),当哪些地方需要用到这个实现类作为依赖时,就可以注入了.
 // 当然我们也可以主动给这个实现类命名,在括号中提供名字 默认是类名首字母小写 @Repository(studentsMapper)
+
+@Repository
 public interface StudentsMapper {
     /**
      * @return Student 返回所有学生的信息
